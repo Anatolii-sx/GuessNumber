10e40b4fe8a292b7928137331755eddc07ca180d
Anatolii-sx <s8.8@bk.ru>
2022-06-23 22:45:56 +0300

    #11 ResultVC and ResultPresenter are ready

--- GuessNumber/Presenters/ResultPresenter.swift
+++ GuessNumber/Presenters/ResultPresenter.swift
@@ -10,13 +10,32 @@ import Foundation
 // MARK: - ResultPresenterProtocol (Connection View -> Presenter)
 protocol ResultPresenterProtocol {
     init(view: ResultViewProtocol)
+    func getTheWinner(triesComputer: Int, triesPlayer: Int)
 }
 
 class ResultPresenter: ResultPresenterProtocol {
+    
+    // MARK: - Private Properties
     private unowned let view: ResultViewProtocol
     
     // MARK: - Realization ResultPresenterProtocol Init (Connection View -> Presenter)
     required init(view: ResultViewProtocol) {
         self.view = view
+    }
+    
+    // MARK: - Realization ResultPresenterProtocol Methods (Connection View -> Presenter)
+    func getTheWinner(triesComputer: Int, triesPlayer: Int) {
+        view.changeTriesCountLabels(
+            playerScore: "Your's tries count: \(triesPlayer)",
+            computerScore: "Computer's tries count: \(triesComputer)"
+        )
+        
+        if triesPlayer == triesComputer {
+            view.changeWinnerLabel(text:"Nobody Win")
+        } else if triesPlayer < triesComputer {
+            view.changeWinnerLabel(text:"You Win")
+        } else if triesPlayer > triesComputer {
+            view.changeWinnerLabel(text:"Computer Win")
+        }
     }
 }
--- GuessNumber/ViewControllers/ResultViewController.swift
+++ GuessNumber/ViewControllers/ResultViewController.swift
@@ -9,43 +9,39 @@ import UIKit
 
 // MARK: - ResultViewProtocol (Connection Presenter -> View)
 protocol ResultViewProtocol: AnyObject {
-    
+    func changeTriesCountLabels(playerScore: String, computerScore: String)
+    func changeWinnerLabel(text: String)
 }
 
 class ResultViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var triesPlayerLabel: UILabel!
+    @IBOutlet weak var triesComputerLabel: UILabel!
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    // MARK: - Public Properties
     var triesComputer: Int!
     var triesPlayer: Int!
     
     // MARK: - Private Properties
     private var presenter: ResultPresenterProtocol!
     
-    @IBOutlet weak var triesPlayerLabel: UILabel!
-    @IBOutlet weak var triesComputerLabel: UILabel!
-    @IBOutlet weak var winnerLabel: UILabel!
-    
+    // MARK: - Methods Of ViewController's Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        triesPlayerLabel.text = "Your's tries count: \(triesPlayer ?? 0)"
-        triesComputerLabel.text = "Computer's tries count: \(triesComputer ?? 0)"
-        showTheWinner()
-        
         presenter = ResultPresenter(view: self)
-    }
-
-    func showTheWinner() {
-        winnerLabel.isHidden = false
-        if triesPlayer == triesComputer {
-            winnerLabel.text = "Nobody Win"
-        } else if triesPlayer < triesComputer {
-            winnerLabel.text = "You Win"
-        } else if triesPlayer > triesComputer {
-            winnerLabel.text = "Computer Win"
-        }
+        presenter.getTheWinner(triesComputer: triesComputer, triesPlayer: triesPlayer)
     }
 }
 
 // MARK: - Realization ResultViewProtocol Methods (Connection Presenter -> View)
 extension ResultViewController: ResultViewProtocol {
+    func changeTriesCountLabels(playerScore: String, computerScore: String) {
+        triesPlayerLabel.text = playerScore
+        triesComputerLabel.text = computerScore
+    }
     
+    func changeWinnerLabel(text: String) {
+        winnerLabel.text = text
+    }
 }
